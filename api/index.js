678c2085f354712bf39c8e939eeeefb44dc58096
Rahul <thakur.rahul1906@gmail.com>
2022-08-08 23:07:37 +0530

    Second phase

--- api/index.js
+++ api/index.js
@@ -1,15 +1,32 @@
-const express =require('express')
-const app = require('express')()
-const mongoose = require('mongoose')
-const dotenv =require('dotenv')
-const authRoute = require('./routes/auth')
-dotenv.config()
-mongoose.connect(process.env.MONGO,{
-    useNewUrlParser:true,
-    useUnifiedTopology : true,
-}).then(()=>console.log("Db connection Succesful")).catch((err)=>console.log(err))
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const movieRoute = require("./routes/movie");
+const listRoute = require("./routes/list");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
 app.use(express.json());
-app.use("/api/auth",authRoute);
-app.listen(3000,()=>{
-    console.log("App is running");
-})
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
